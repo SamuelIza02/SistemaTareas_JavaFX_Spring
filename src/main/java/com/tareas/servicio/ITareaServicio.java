bae97c8eef7a0b3ce70245d5aa3179c50bf7dbfd
Samuel <samuel.iza@outlook.com>
2025-07-07 13:55:25 -0600

    Creacion packages repositorio, servicio con sus funciones

---
+++ src/main/java/com/tareas/servicio/ITareaServicio.java
@@ -0,0 +1,12 @@
+package com.tareas.servicio;
+
+import com.tareas.modelo.Tarea;
+
+import java.util.List;
+
+public interface ITareaServicio {
+    public List<Tarea> listarTareas();
+    public Tarea buscarTareaPorId(Integer idTarea);
+    public void guardarTarea(Tarea tarea);
+    public void eliminarTarea(Tarea tarea);
+}
